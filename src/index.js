f24f539e5eac0124ba76473add1d1d46d7443ace
super-gr0ver <e.dorodnov@gmail.com>
2024-09-01 23:35:46 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/gsgupanelpf">
+    <BrowserRouter basename="/gsgupanelspf">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
